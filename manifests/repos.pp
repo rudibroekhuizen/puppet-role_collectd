e3c708966b021492ccedaddd4235fcdc7783fb34
Rudi Broekhuizen <rudi.broekhuizen@naturalis.nl>
2014-10-14 13:14:28 +0200

    Update repos.pp

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -16,8 +16,15 @@ class role_collectd::repos {
     
     
     redhat: {
-      notify { "continue": 
+      yumrepo { 'pakk':
+        descr    => 'pakk Centos Repo',
+        baseurl  => "http://pakk.96b.it/repositories/el6/x86_64/",
+        gpgcheck => 1,
+        gpgkey   => 'http://pakk.96b.it/RPM-GPG-KEY-pakk',
+        enabled  => 1,
       }
+      #notify { "continue": 
+      #}
     }
   }
 }
